2c2247f50953bca08e555504caf712a00595e791
Andrew R Emery <hello@andrewremery.com>
2023-08-15 18:33:37 -0400

    :sparkles: Mobile nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Script file has been loaded");
+(function ($) {
+  function createMobileMenuButton() {
+    var mobileMenuButton = $('<button id="mobile-menu-button">Menu</button>');
+    mobileMenuButton.prependTo($("#main-menu"));
+  }
+  function toggleMobileMenu() {
+    $("#main-menu ul").slideToggle();
+  }
+  
+  $(document).ready(function () {
+    createMobileMenuButton();
+    $("#mobile-menu-button").on("click", () => {
+      toggleMobileMenu();
+    });
+  });
+})(jQuery);
